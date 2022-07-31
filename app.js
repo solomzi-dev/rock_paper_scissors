62c1e00c0b3008004f805db8f4ad7471f8ebce0c
Solomzi <solomzi.dev@gmail.com>
2022-07-31 19:45:32 +0200

    Add playRound function

--- app.js
+++ app.js
@@ -3,3 +3,27 @@ function getComputerChoice() {
     const rand = Math.floor(Math.random() * choices.length);
     return choices[rand];
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
+        return `It's a tie.`;
+    } else if (playerSelection.toLowerCase() === 'rock' && computerSelection.toLowerCase() === 'scissors') {
+        return `You win! Rock beats Scissors.`;
+    } else if (playerSelection.toLowerCase() === 'paper' && computerSelection.toLowerCase() === 'rock') {
+        return `You win! Paper beats Rock.`;
+    } else if (playerSelection.toLowerCase() === 'scissors' && computerSelection.toLowerCase() === 'paper') {
+        return `You win! Scissors beats Paper.`;
+    } else if (computerSelection.toLowerCase() === 'rock' && playerSelection.toLowerCase() === 'scissors') {
+        return `You lose! Rock beats Scissors.`;
+    } else if (computerSelection.toLowerCase() === 'paper' && playerSelection.toLowerCase() === 'rock') {
+        return `You lose! Paper beats Rock.`;
+    } else if (computerSelection.toLowerCase() === 'scissors' && playerSelection.toLowerCase() === 'paper') {
+        return `You lose! Scissors beat Paper.`;
+    } else {
+        return 'Invalid input';
+    }
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
